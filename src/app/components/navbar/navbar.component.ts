c97654e9f494c6edef7ff842b392bda6c451a2f7
JokerV <Quocvietcoi2000@gmail.com>
2020-01-01 00:20:06 +0700

    Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
+  isShowSearchInput = false;
+  isCollapsedAnimate = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onShowSearchInput() {
+    this.isShowSearchInput = !this.isShowSearchInput;
   }
 
+  onCollapsedNavbar() {
+    this.isCollapsedAnimate = !this.isCollapsedAnimate;
+  }
 }

d114fc4ab405351ae7545b0111be828ac96050d5
JokerV <Quocvietcoi2000@gmail.com>
2020-01-10 11:07:12 +0700

    Update navbar and fix color

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: './layouts/client/client.module#ClientModule'
-  },
+    path: "",
+    loadChildren: "./layouts/client/client.module#ClientModule"
+  }
   // {
   //   path: '404',
   //   loadChildren: './layouts/not-found/not-found.module#NotFoundModule'
@@ -14,9 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
